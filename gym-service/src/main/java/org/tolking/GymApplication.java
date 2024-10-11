9ee34b44aa34b3986026b0ebe1948968502c1292
To'lqin <tolking0101@gmail.com>
2024-10-11 12:32:32 +0500

    feat: transform into microservice

--- gym-service/src/main/java/org/tolking/GymApplication.java
+++ gym-service/src/main/java/org/tolking/GymApplication.java
@@ -2,9 +2,10 @@ package org.tolking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GymApplication {
     public static void main(String[] args) {
         SpringApplication.run(GymApplication.class);
