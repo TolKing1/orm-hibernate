be977dc22e42df7d0b6d9aea8008be2c76d93968
To'lqin <tolking0101@gmail.com>
2024-11-01 21:24:08 +0500

    feat: added trace id to messaging
    (cherry picked from commit 0fff700dba628b43a75fcef3f43fd3efd007be15)

--- training-tracker-service/src/main/java/org/tolking/messaging/TrackerEventConsumer.java
+++ training-tracker-service/src/main/java/org/tolking/messaging/TrackerEventConsumer.java
@@ -2,10 +2,14 @@ package org.tolking.messaging;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 import org.tolking.service.TrainingEventService;
 import org.tolking.external_dto.TrainingEventDTO;
+
+import static org.tolking.filter.LoggingFilter.CORRELATION_ID;
 
 @Component
 @RequiredArgsConstructor
@@ -15,8 +19,14 @@ public class TrackerEventConsumer {
     private final static String QUEUE_NAME = "training-event-queue";
 
     @JmsListener(destination = QUEUE_NAME)
-    public void receiveMessage(TrainingEventDTO trainingEventDTO) {
+    public void receiveMessage(TrainingEventDTO trainingEventDTO, @Header(CORRELATION_ID) String correlationId) {
+        addTraceId(correlationId);
+
         log.debug("Received message from queue {} : {}", QUEUE_NAME, trainingEventDTO);
         trainingEventService.create(trainingEventDTO);
+    }
+
+    private static void addTraceId(String correlationId) {
+        MDC.put(CORRELATION_ID, correlationId);
     }
 }
